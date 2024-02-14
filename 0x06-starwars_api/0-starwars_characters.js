1c0cce1030104c2505e70d9400310313766c66a7
robel <robelwon75@gmail.com>
2024-02-14 20:59:16 +0300

    bug fixes

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -7,12 +7,11 @@ request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err,
   const k = JSON.parse(body).characters;
   exactOrder(k, 0);
 });
-
-const exactOrder = (k, x) => {
-  if (x === k.length) return;
-  request(k[x], function (err, res, body) {
+const exactOrder = (k, num) => {
+  if (num === k.length) return;
+  request(k[num], function (err, res, body) {
     if (err) throw err;
     console.log(JSON.parse(body).name);
-    exactOrder(k, x + 1);
+    exactOrder(k, num + 1);
   });
 };
